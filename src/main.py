7a4f58bd0bb04749c6cbfc6a430cc6329bd6a43f
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2024-03-22 17:04:45 +0200

    Update UI, add game field creation, add UI handlers

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-"""Example module providing the user interface for the game."""
-
 import wx
 import ui
 
-if __name__ == '__main__':
+def main():
     app = wx.App()
-    window = ui.MainWindow(None)
-    window.Show(True)
-
-    dialog = ui.GameStartDialog(window)
-    dialog.Show()
+    ui.MainWindow(None)
     app.MainLoop()
+
+if __name__ == '__main__':
+    main()
--- src/ui.py
+++ src/ui.py
@@ -1,256 +1,127 @@
-# -*- coding: utf-8 -*-
-
-###########################################################################
-## Python code generated with wxFormBuilder (version 4.1.0-0-g733bf3d-dirty)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO *NOT* EDIT THIS FILE!
-###########################################################################
-
 import wx
-import wx.xrc
+import ui_generated
+import game
 
-###########################################################################
-## Class MainWindow
-###########################################################################
+class MainWindow(ui_generated.MainWindow):
+    def __init__(self, parent):
+        ui_generated.MainWindow.__init__(self, parent)
 
-class MainWindow ( wx.Frame ):
+        self.player_type = "O"
+        self.active_button = None
+        self.game_complete = True
+        self.Show()
 
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 640,240 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        self.start_dialog = GameStartDialog(self)
+        self.start_dialog.Show()
 
-		self.SetSizeHints( wx.Size( 640,240 ), wx.DefaultSize )
-		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_MENU ) )
+    def get_game_field(self):
+        return self.game_field_panel.GetSizer().GetChildren()[0].Sizer
 
-		bSizer4 = wx.BoxSizer( wx.VERTICAL )
+    def init_game_field(self, game_str: str):
+        game_field = self.get_game_field()
+        game_field.Clear(True)
 
-		sbSizer4 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Punkti" ), wx.VERTICAL )
+        for character in game_str:
+            button = wx.ToggleButton(
+                self.game_field_panel,
+                label=character,
+                pos=wx.DefaultPosition,
+                size=(24, 24),
+                style=wx.BORDER_NONE)
 
-		bSizer71 = wx.BoxSizer( wx.HORIZONTAL )
+            button.Bind(wx.EVT_TOGGLEBUTTON, self.on_game_field_button_clicked)
+            game_field.Add(button, 0, wx.ALIGN_CENTER_VERTICAL, 5)
 
-		self.m_staticText2 = wx.StaticText( sbSizer4.GetStaticBox(), wx.ID_ANY, u"Krusts:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText2.Wrap( -1 )
+        self.game_field_panel.Layout()
+        self.game_complete = False
 
-		bSizer71.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+    def on_game_field_button_clicked(self, event):
+        button = event.EventObject
 
-		self.m_staticText3 = wx.StaticText( sbSizer4.GetStaticBox(), wx.ID_ANY, u"0", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText3.Wrap( -1 )
+        if not button.GetNextSibling():
+            button.Value = False
+            return
 
-		bSizer71.Add( self.m_staticText3, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+        if button.Value:
+            if self.active_button:
+                self.active_button.Value = False
+                self.active_button.GetNextSibling().Value = False
 
+            button.Value = False
 
-		bSizer71.Add( ( 120, 0), 0, 0, 5 )
+            if self.player_type != button.Label:
+                button.Value = True
+                button.GetNextSibling().Value = True
+                self.active_button = button
+        else:
+            if button == self.active_button:
+                button.GetNextSibling().Value = False
+            else:
+                self.active_button.Value = False
 
-		self.m_staticText5 = wx.StaticText( sbSizer4.GetStaticBox(), wx.ID_ANY, u"Aplis:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText5.Wrap( -1 )
+    def on_perform_move_clicked(self, event):
+        if self.game_complete:
+            return
 
-		bSizer71.Add( self.m_staticText5, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+        button = self.active_button
 
-		self.m_staticText6 = wx.StaticText( sbSizer4.GetStaticBox(), wx.ID_ANY, u"0", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText6.Wrap( -1 )
+        if button and button.Value:
+            button.GetNextSibling().Destroy()
+            button.Label = self.player_type
+            button.Value = False
 
-		bSizer71.Add( self.m_staticText6, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+            if self.player_type == "O":
+                self.player_type = "X"
+            else:
+                self.player_type = "O"
 
+            self.active_button = None
+            self.game_field_panel.Layout()
 
-		sbSizer4.Add( bSizer71, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+        if self.is_game_complete():
+            self.on_game_completed()
 
+    def on_new_game_clicked(self, event):
+        self.start_dialog.Show()
 
-		bSizer4.Add( sbSizer4, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
-		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.BORDER_STATIC|wx.TAB_TRAVERSAL )
-		bSizer9 = wx.BoxSizer( wx.VERTICAL )
+    def is_game_complete(self):
+        buttons = self.game_field_panel.GetChildren()
+        count = len(buttons)
 
-		bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
+        if count <= 1:
+            return True
 
-		self.m_toggleBtn1 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn1, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
+        for i in range(count - 1):
+            if buttons[i].Label != self.player_type:
+                return False
 
-		self.m_toggleBtn2 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn2, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
+        return True
 
-		self.m_toggleBtn3 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn3, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn4 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn4, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn5 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn5, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn6 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn6, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn7 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn7, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn8 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn8, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn9 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn9, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn10 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn10, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn11 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn11, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn12 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn12, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn13 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn13, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn14 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn14, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn15 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn15, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn16 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn16, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn17 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn17, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn18 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn18, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn19 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn19, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn20 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn20, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn21 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn21, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn22 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn22, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn23 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn23, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn24 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"X", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn24, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_toggleBtn25 = wx.ToggleButton( self.m_panel2, wx.ID_ANY, u"O", wx.DefaultPosition, wx.Size( 24,24 ), wx.BORDER_NONE )
-		bSizer6.Add( self.m_toggleBtn25, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
-
-
-		bSizer9.Add( bSizer6, 1, wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		self.m_panel2.SetSizer( bSizer9 )
-		self.m_panel2.Layout()
-		bSizer9.Fit( self.m_panel2 )
-		bSizer4.Add( self.m_panel2, 1, wx.ALL|wx.EXPAND, 5 )
-
-		bSizer7 = wx.BoxSizer( wx.VERTICAL )
-
-		self.m_button10 = wx.Button( self, wx.ID_ANY, u"Izpildīt gājienu", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer7.Add( self.m_button10, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-		self.m_button2 = wx.Button( self, wx.ID_ANY, u"Sākt jaunu spēli", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer7.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		bSizer4.Add( bSizer7, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		self.SetSizer( bSizer4 )
-		self.Layout()
-
-		self.Centre( wx.BOTH )
-
-	def __del__( self ):
-		pass
-
-
-###########################################################################
-## Class GameStartDialog
-###########################################################################
-
-class GameStartDialog ( wx.Dialog ):
-
-	def __init__( self, parent ):
-		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 350,350 ), style = wx.DEFAULT_DIALOG_STYLE )
-
-		self.SetSizeHints( wx.Size( 350,350 ), wx.DefaultSize )
-
-		bSizer2 = wx.BoxSizer( wx.VERTICAL )
-
-		bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
-
-		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Spēles garums:", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText2.Wrap( -1 )
-
-		bSizer3.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
-		self.m_textCtrl2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer3.Add( self.m_textCtrl2, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
-
-		bSizer2.Add( bSizer3, 0, wx.ALL|wx.EXPAND, 5 )
-
-		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Spēlēt ar" ), wx.HORIZONTAL )
-
-		gSizer2 = wx.GridSizer( 1, 2, 0, 0 )
-
-		self.m_radioBtn5 = wx.RadioButton( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Apļiem", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer2.Add( self.m_radioBtn5, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-		self.m_radioBtn6 = wx.RadioButton( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Krustiem", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer2.Add( self.m_radioBtn6, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		sbSizer3.Add( gSizer2, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
-
-		bSizer2.Add( sbSizer3, 1, wx.ALL|wx.EXPAND, 5 )
-
-		sbSizer4 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Spēlēt pret" ), wx.HORIZONTAL )
-
-		gSizer3 = wx.GridSizer( 0, 2, 0, 0 )
-
-		self.m_radioBtn7 = wx.RadioButton( sbSizer4.GetStaticBox(), wx.ID_ANY, u"Cilvēku", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer3.Add( self.m_radioBtn7, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-		self.m_radioBtn8 = wx.RadioButton( sbSizer4.GetStaticBox(), wx.ID_ANY, u"Datoru", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer3.Add( self.m_radioBtn8, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		sbSizer4.Add( gSizer3, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
-
-		bSizer2.Add( sbSizer4, 1, wx.ALL|wx.EXPAND, 5 )
-
-		sbSizer5 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Datora algoritms" ), wx.HORIZONTAL )
-
-		gSizer4 = wx.GridSizer( 1, 2, 0, 0 )
-
-		self.m_radioBtn9 = wx.RadioButton( sbSizer5.GetStaticBox(), wx.ID_ANY, u"Minimaksa", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer4.Add( self.m_radioBtn9, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-		self.m_radioBtn10 = wx.RadioButton( sbSizer5.GetStaticBox(), wx.ID_ANY, u"Alfa-beta", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer4.Add( self.m_radioBtn10, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
-
-
-		sbSizer5.Add( gSizer4, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
-
-
-		bSizer2.Add( sbSizer5, 1, wx.ALL|wx.EXPAND, 5 )
-
-		self.m_button1 = wx.Button( self, wx.ID_ANY, u"Sākt spēli", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer2.Add( self.m_button1, 0, wx.ALL|wx.EXPAND, 5 )
-
-
-		self.SetSizer( bSizer2 )
-		self.Layout()
-
-		self.Centre( wx.BOTH )
-
-	def __del__( self ):
-		pass
-
-
+    def on_game_completed(self):
+        self.game_complete = True
+        print("done")
+        # Game completion code here
+
+class GameStartDialog(ui_generated.GameStartDialog):
+    def get_selected_player_type(self):
+        if self.circle_radio_btn.Value:
+            return game.GameAlgorithm.MINIMAX
+
+        return game.GameAlgorithm.ALFABETA
+
+    def get_selected_algorithm(self):
+        if self.minimax_radio_btn.Value:
+            return game.PlayerType.CIRCLE
+
+        return game.PlayerType.CROSS
+
+    def on_start_game_clicked(self, event):
+        game.set_settings(
+            int(self.length_num_box.Value),
+            self.get_selected_player_type(),
+            self.get_selected_algorithm())
+
+        self.Parent.init_game_field(game.temp_get_game_state())
+        self.Hide()
+        event.Skip()
