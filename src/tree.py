e422239159cd86099b4298b8c58f60a90aedb677
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2024-04-03 08:59:29 +0300

    Remove redundant tree code

--- src/tree.py
+++ src/tree.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.parent = parent
         self.cross_points = 0 if not parent else parent.cross_points
         self.circle_points = 0 if not parent else parent.circle_points
+        self.changed_index = -1
 
 
 def opposite_player(player):
@@ -18,6 +19,7 @@ def new_child(node, i, circle_points_diff, cross_points_diff):
     child = TreeNode(new_state, opposite_player(node.player_type), node)
     child.circle_points += circle_points_diff
     child.cross_points += cross_points_diff
+    child.changed_index = i
     node.children.append(child)
 
     return child
@@ -28,14 +30,14 @@ def generate_tree(node, depth):
         return
 
     for i in range(len(node.state) - 1):
-        if node.player_type == 'O':
-            if node.state[i:i+2] == 'XX':
-                generate_tree(new_child(node, i, 2, 0), depth - 1)
-            elif node.state[i:i+2] == 'XO':
-                generate_tree(new_child(node, i, 0, -1), depth - 1)
+        player = node.player_type
+        move = node.state[i:i+2]
 
-        elif node.player_type == 'X':
-            if node.state[i:i+2] == 'OO':
-                generate_tree(new_child(node, i, 0, 1), depth - 1)
-            elif node.state[i:i+2] == 'OX':
-                generate_tree(new_child(node, i, -1, 0), depth - 1)
+        if player == 'O' and move == 'XX':
+            generate_tree(new_child(node, i, 2, 0), depth - 1)
+        elif player == 'O' and move == 'XO':
+            generate_tree(new_child(node, i, 0, -1), depth - 1)
+        elif player == 'X' and move == 'OO':
+            generate_tree(new_child(node, i, 0, 1), depth - 1)
+        elif player == 'X' and move == 'OX':
+            generate_tree(new_child(node, i, -1, 0), depth - 1)
