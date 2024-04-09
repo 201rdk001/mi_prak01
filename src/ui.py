bd52f0d960d9bec31dd41ff16533d5dbfa7bb699
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2024-04-09 22:54:25 +0300

    Revert "Fix attempt for the previous bug"
    This reverts commit 9c26ccd3acd7edf7f4817be13b16eb55f396e739.

--- src/ui.py
+++ src/ui.py
@@ -52,8 +52,6 @@ class MainWindow(ui_generated.MainWindow):
         if self.chosen_player != 'O':
             self.game_field_panel.Disable()
             self.perform_move_button.Disable()
-            self.new_game_button.Disable()
-            self.game_field_panel.Update()
             threading.Thread(target=self.perform_computer_move, daemon=True).start()
 
     def perform_move(self, button):
@@ -72,20 +70,10 @@ class MainWindow(ui_generated.MainWindow):
 
         if self.game.has_ended:
             self.on_game_completed()
-            return True
-
-        if self.game.player != self.chosen_player:
+        elif self.game.player != self.chosen_player:
             self.game_field_panel.Disable()
             self.perform_move_button.Disable()
-            self.new_game_button.Disable()
-            self.game_field_panel.Update()
             threading.Thread(target=self.perform_computer_move, daemon=True).start()
-        else:
-            self.game_field_panel.Enable()
-            self.perform_move_button.Enable()
-            self.new_game_button.Enable()
-
-        return False
 
     def perform_person_move(self):
         if self.active_button and self.active_button.Value:
@@ -106,19 +94,10 @@ class MainWindow(ui_generated.MainWindow):
 
     def end_computer_move_animation(self, button):
         button.BackgroundColour = wx.NullColour
-        ended = self.perform_move(button)
-        self.active_button = None
-
-        if ended:
-            self.game_field_panel.Disable()
-            self.perform_move_button.Disable()
-            self.new_game_button.Disable()
-            self.game_field_panel.Update()
-            return
-
-        self.game_field_panel.Enable()
+        self.perform_move(button)
         self.perform_move_button.Enable()
-        self.new_game_button.Enable()
+        self.game_field_panel.Enable()
+        self.active_button = None
 
     def on_game_field_button_clicked(self, event):
         button: wx.ToggleButton = event.EventObject
@@ -148,7 +127,7 @@ class MainWindow(ui_generated.MainWindow):
         self.perform_person_move()
 
     def on_new_game_clicked(self, event):
-        self.start_dialog.ShowModal()
+        self.start_dialog.Show()
 
     def on_game_completed(self):
         self.active_player_label.SetLabel(str(self.game.player))
